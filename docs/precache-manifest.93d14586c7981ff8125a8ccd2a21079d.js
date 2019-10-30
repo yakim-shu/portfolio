da24e17415feaf48d1e8e486e1b9c55bc9e0685c
yakim shu <yakim-shu@users.noreply.github.com>
2019-10-30 13:39:35 +0800

    Add build folder

---
+++ docs/precache-manifest.93d14586c7981ff8125a8ccd2a21079d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff859bc63d4fef6d6435670d30d9ced",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c9318886e9b0c9155c91",
+    "url": "/portfolio/static/css/2.65d0fb34.chunk.css"
+  },
+  {
+    "revision": "a85ee0a2b636fbd03dd2",
+    "url": "/portfolio/static/css/main.e72432a4.chunk.css"
+  },
+  {
+    "revision": "c9318886e9b0c9155c91",
+    "url": "/portfolio/static/js/2.94318f96.chunk.js"
+  },
+  {
+    "revision": "a85ee0a2b636fbd03dd2",
+    "url": "/portfolio/static/js/main.d8175713.chunk.js"
+  },
+  {
+    "revision": "3a5bc68b49cd72246a16",
+    "url": "/portfolio/static/js/runtime~main.452581ef.js"
+  }
+]);
